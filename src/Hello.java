0d42bb2d46f56deeb0e0b25f0125b0db75b6c267
Jay Janodia <jay.janodia@gmail.com>
2023-03-07 14:02:26 -0800

    Added Logical And Operator example

--- src/Hello.java
+++ src/Hello.java
@@ -4,6 +4,17 @@ public class Hello {
         boolean isAlien = false;
         if (isAlien == false) {
             System.out.println("It is not an alien");
+            System.out.println("And I am not scared of aliens");
+        }
+
+        int topScore = 99;
+        if (topScore >= 100) {
+            System.out.println("You got the high score");
+        }
+
+        int secondTopScore = 60;
+        if (topScore < 100 && topScore > secondTopScore) {
+            System.out.println("Greater than second top score and less than 100");
         }
     }
 }
