a74d0177eb2584d0ec9053930d4677fa7b2c1837
Jay Janodia <jay.janodia@gmail.com>
2023-03-07 13:32:58 -0800

    Wrote a simple code to print "Hello World"

---
+++ src/Hello.java
@@ -0,0 +1,9 @@
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("Hello, Jay Janodia");
+        boolean isAlien = false;
+        if (isAlien == false) {
+            System.out.println("It is not an alien");
+        }
+    }
+}
